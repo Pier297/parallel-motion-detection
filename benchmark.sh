1790eba4834e2ab126381f5d36bfba4e666d0759
Pier Paolo Tarasco <p.tarasco@spm.polo2.ad.unipi.it>
2022-08-31 09:57:39 +0200

    final results

--- benchmark.sh
+++ benchmark.sh
@@ -10,10 +10,13 @@ fi
 
 # <n> <output_path> <pin_threads> <blocking_mode> <video_path_1> <k_1> <res_1> ... <video_path_l> <k_l> <res_l>
 # 3x3
-./bin/Benchmark $NUM_REPEATS "kernel_3x3/" 0 0 "videos/small_video.mp4" 0.3 722 "videos/large_video.mp4" 0.3 571
-./bin/Benchmark $NUM_REPEATS "kernel_3x3/" 0 1 "videos/small_video.mp4" 0.3 722 "videos/large_video.mp4" 0.3 571
-./bin/Benchmark $NUM_REPEATS "kernel_3x3/" 1 0 "videos/small_video.mp4" 0.3 722 "videos/large_video.mp4" 0.3 571
-./bin/Benchmark $NUM_REPEATS "kernel_3x3/" 1 1 "videos/small_video.mp4" 0.3 722 "videos/large_video.mp4" 0.3 571
+# ./bin/Benchmark $NUM_REPEATS "kernel_3x3_00/" 0 0 "videos/small_video.mp4" 0.3 722 "videos/large_video.mp4" 0.3 571
+# ./bin/Benchmark $NUM_REPEATS "kernel_3x3_01/" 0 1 "videos/small_video.mp4" 0.3 722 "videos/large_video.mp4" 0.3 571
+# ./bin/Benchmark $NUM_REPEATS "kernel_3x3_10/" 1 0 "videos/small_video.mp4" 0.3 722 "videos/large_video.mp4" 0.3 571
+# ./bin/Benchmark $NUM_REPEATS "kernel_3x3_11/" 1 1 "videos/small_video.mp4" 0.3 722 "videos/large_video.mp4" 0.3 571
 
 # 9x9 (REMEMBER TO RE-COMPILE)
-#./bin/Benchmark $NUM_REPEATS "kernel_9x9/" 0 0 "videos/small_video.mp4" 0.3 812 "videos/large_video.mp4" 0.3 629
+./bin/Benchmark $NUM_REPEATS "kernel_9x9_00/" 0 0 "videos/small_video.mp4" 0.3 812 "videos/large_video.mp4" 0.3 629
+./bin/Benchmark $NUM_REPEATS "kernel_9x9_01/" 0 1 "videos/small_video.mp4" 0.3 812 "videos/large_video.mp4" 0.3 629
+./bin/Benchmark $NUM_REPEATS "kernel_9x9_10/" 1 0 "videos/small_video.mp4" 0.3 812 "videos/large_video.mp4" 0.3 629
+./bin/Benchmark $NUM_REPEATS "kernel_9x9_11/" 1 1 "videos/small_video.mp4" 0.3 812 "videos/large_video.mp4" 0.3 629
--- src/OptimizedUtils.cpp
+++ src/OptimizedUtils.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 using namespace cv;
 
-#define kernel_size 3
-#define pad 1
+#define kernel_size 9
+#define pad 4
 // pad = floor(kernel_size / 2)
 
 vector<vector<short int>> grayscale(const Mat & frame)
